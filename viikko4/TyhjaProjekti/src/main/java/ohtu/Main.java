26b6df6bc0ce702e44035b35390c4c1f3c52961c
Vornanen Riku H <rivorivo@da3-cs-dk108-05.helsinki.fi>
2016-04-17 15:46:13 +0300

    tagikommit

--- viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,7 +1,47 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.io.IOUtils;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String studentNr = "014029624";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "http://ohtustats2016.herokuapp.com/students/"+studentNr+"/submissions";
+
+        HttpClient client = new HttpClient();
+        GetMethod method = new GetMethod(url);
+        client.executeMethod(method);
+
+        InputStream stream =  method.getResponseBodyAsStream();
+
+        String bodyText = IOUtils.toString(stream);
+
+        System.out.println("json-muotoinen data:");
+        System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        System.out.println("Oliot:");
+        int teht=0;
+        int tunnit=0;
+        for (Submission subi : subs) {
+            
+            System.out.println("opiskelijanumero: "+subi.getStudent_number());
+            System.out.println("viikko "+subi.getWeek()+": tehtyjä tehtäviä yhteensä: "+subi.getTehdytTehtMaara()+", aikaa kului "+subi.getHours()+" tuntia, tehdyt tehtävät:"+subi.getTehdytTeht());
+            System.out.println("");   
+            teht+=subi.getTehdytTehtMaara();
+            tunnit+=Integer.parseInt(subi.getHours());
+        }
+        System.out.println("yhteensä: "+teht+" tehtävää "+tunnit+" tuntia");
     }
 }
--- viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -1,19 +1,78 @@
 package ohtu;
 
+
+import java.util.ArrayList;
+
+
 public class Submission {
+
     private String student_number;
+    private String week;
+    private String hours;
+    private ArrayList<Integer> tehdyt=new ArrayList<Integer>();
+    private String a1,a2,a3,a4,a5,a6,a7,a8;
+
+    public void setTehdytTeht() {
+       if (a1.equals("true")) {
+           tehdyt.add(1);
+        }       
+       if(a2.equals("true")){
+           tehdyt.add(2);
+       }
+        if(a3.equals("true")){
+           tehdyt.add(3);
+       }
+       if(a4.equals("true")){
+           tehdyt.add(4);
+       }
+        if(a5.equals("true")){
+           tehdyt.add(5);
+       }
+        if(a6.equals("true")){
+           tehdyt.add(6);
+       }
+        if(a7.equals("true")){
+           tehdyt.add(7);
+       }
+        if(a8.equals("true")){
+           tehdyt.add(8);
+       }   
+        
+    }
+    
+    
+    
+    public ArrayList getTehdytTeht(){
+      
+        setTehdytTeht();
+  
+        return tehdyt;
+    }
+   
+ 
+    public int getTehdytTehtMaara(){
+        setTehdytTeht();
+       return tehdyt.size();
+    }
 
     public String getStudent_number() {
         return student_number;
     }
-
-    public void setStudent_number(String student_number) {
-        this.student_number = student_number;
+ 
+    public String getWeek(){
+        return week;
     }
 
+    public String getHours(){
+        return hours;
+    }
+    
+  
+    
     @Override
     public String toString() {
         return student_number;
     }
+     
     
 }
